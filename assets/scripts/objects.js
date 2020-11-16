4bb03d5163b8c5d0601ab1cd775c33cd858652d7
Gagan Preet <ggnpreet2301@gmail.com>
2020-11-16 23:05:44 +0530

    display entered movie

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -2,6 +2,33 @@ const addMovieBtn = document.getElementById("add-movie-btn");
 const searchBtn = document.getElementById("search-btn");
 const movies = [];
 
+
+const renderMovies = () => {
+    const movieList = document.getElementById("movie-list");
+
+    if (movies.length === 0) {
+        movieList.classList.remove("visible");
+        return;
+    } else {
+        movieList.classList.add("visible");
+    }
+    movieList.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const movieEl = document.createElement("li");
+        // movieEl.textContent = movie.info.title;
+        let text = movie.info.title;
+        for (const key in movie.info) {
+            if (key !== 'title') {
+                text = text + ` - ${key}:${movie.info[key]}`
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+
+    });
+};
+
 const addMovieHandler = () => {
     const title = document.getElementById("title").value;
     const extraName = document.getElementById("extra-name").value;
@@ -18,8 +45,8 @@ const addMovieHandler = () => {
         id: Math.random()
     };
     movies.push(newMovie);
-    // console.log(newMovie);
-    console.log(movies);
+    renderMovies();
+
     document.getElementById("title").value = "";
     document.getElementById("extra-name").value = "";
     document.getElementById("extra-value").value = "";
